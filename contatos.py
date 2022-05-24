7769604de796a171071274808dbe5d1ea1ccc202
NinjaBitroom <gabrielbitercourt@gmail.com>
2022-05-24 14:57:29 -0400

    Adicione um menu no programa

--- contatos.py
+++ contatos.py
@@ -10,22 +10,45 @@ valor_hora = []
 
 resposta = ''
 contatos = 0
-while resposta.upper() == 'S' or resposta == '':
-    while resposta.upper() not in ('S', 'N'):
-        resposta = input('Deseja criar um novo contato? (S/N): ')
-        if resposta.upper() not in ('S', 'N'):
-            print('Não entendi a resposta, responda com "S" ou "N"!')
-    
-    if resposta.upper() == 'S':
+print('Bem vindo ao projAgenda!')
+while True:
+    print('-'*40)
+    print('1 - Criar contato')
+    print('2 - Mudar contato')
+    print('3 - Excluir contato')
+    print('4 - Consultar contato')
+    print('0 - Sair')
+    resposta = input('Escolha uma das opções acima: ')
+
+    if resposta == '0':
+        print('Sair')
+        break
+
+    elif resposta == '1':
+        print('Criar')
         nomes.append(input('Digite o nome do contato: '))
         emails.append(input('Digite o e-mail do contato: '))
         telefones.append(input('Digite o telefone do contato: '))
         valor_hora.append(input('Digite o valor-hora do contato: R$ '))
-        resposta = ''
         contatos += 1
+    
+    elif resposta == '2':
+        print('Mudar')
+        pass
 
-print('='*20)
-print('nome|email|telefone|valor-hora')
-print('-'*20)
+    elif resposta == '3':
+        print('Excluir')
+        pass
+
+    elif resposta == '4':
+        print('Consultar')
+        pass
+
+    else:
+        print('Digite uma das opções acima!')
+
+print('='*40)
+print('nome | email | telefone | valor-hora')
+print('-'*40)
 for contato in range(contatos):
     print(f'{nomes[contato]} | {emails[contato]} | {telefones[contato]} | R$ {valor_hora[contato]}')
